983c46a39d3561977d0df7bf09c60c04737831d1
Angel Martinez <angel.codes139@gmail.com>
2020-07-19 20:07:25 -0500

    Finish the desing

--- template/src/App.js
+++ template/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import tw from 'twin.macro';
 
+// Components
+import Content from './components/Content';
+
 // Styled Components
 const Container = tw.div`min-h-screen flex flex-col items-center justify-center bg-gray-800 text-center`;
 
 function App() {
   return (
     <Container>
-      
+      <Content/>
     </Container>    
   );
 }
--- template/src/components/Content.js
+++ template/src/components/Content.js
@@ -5,9 +5,46 @@ import tw, { css } from 'twin.macro';
 // images
 import logo from '../logo.svg';
 
+// Keyframes
+const animationLogo = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`; 
+
+// Styled Components
+const Paragraph = tw.p`text-3xl font-medium text-white`;
+const LinkGroup = tw.div`flex`;
+const Link = tw.a`text-xl text-blue-200 hover:text-blue-300 font-medium mx-4 cursor-pointer transition-all ease-in duration-300`;
+
+
 const Content = () => {
     return (
-        <h1>From Content</h1>
+        <Fragment>
+            <img src={logo} css={[
+                tw`pointer-events-none`,
+                css`
+                    animation: ${animationLogo} infinite 20s linear;
+                    height: 50vmin;
+                `
+            ]} />
+            <Paragraph>React.js with TailwindCSS & Emotion</Paragraph>
+            <LinkGroup>
+                <Link
+                    href="https://github.com/angel-codes/cra-template-tailwindcss-emotion"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >Github Repository</Link>
+                <Link
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >Learn React</Link>
+            </LinkGroup>
+        </Fragment>
     );
 }
  
