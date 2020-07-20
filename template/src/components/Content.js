a14a0ff04efca7a73f3faeda494ab8634c88d4e0
Angel Martinez <angel.codes139@gmail.com>
2020-07-19 20:06:29 -0500

    import all thing what we needed

--- template/src/components/Content.js
+++ template/src/components/Content.js
@@ -1,4 +1,9 @@
-import React from 'react';
+/** @jsx jsx */ import { jsx, keyframes } from '@emotion/core'
+import { Fragment } from 'react';
+import tw, { css } from 'twin.macro';
+
+// images
+import logo from '../logo.svg';
 
 const Content = () => {
     return (
